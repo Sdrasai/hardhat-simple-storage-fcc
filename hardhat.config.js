798a454fd4c547862dcb2a8fe24e0650277c803d
[sadra sadeghi] <[sadra82.sadeghi@gmail.com]>
2023-10-29 15:24:00 +0330

    hardhat-simple-storage-fcc

---
+++ hardhat.config.js
@@ -0,0 +1,40 @@
+require("@nomiclabs/hardhat-waffle")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks:{
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId:11155111,
+    }
+  },
+  localhost: {
+    url:"http://127.0.0.1:8545",
+    chainId: 31337,
+  },
+  solidity: "0.8.19",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "MATIC"
+  }
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,50 @@
+// imports
+const { constants } = require("ethers")
+const { ethers, run, network } = require("hardhat")
+// async main
+async function main() {
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+    console.log("Deploying Contract...")
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+    console.log(`Deployed contract to: ${simpleStorage.address}`)
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for block txes...")
+        await simpleStorage.deployTransaction.wait(6)
+        await verify(simpleStorage.address, [])
+    }
+
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current Value is: ${currentValue}`)
+
+    const tarnsactionResponse = await simpleStorage.store(7)
+    await tarnsactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated value is: ${updatedValue}`)
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying Contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!")
+        } else {
+            console.log(e)
+        }
+    }
+}
+
+// main
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
